2a0526f521d30ee92a6d112e0562187dbd12b4e2
atthapong31366 <atthapongboat31366@hotmail.com>
2023-10-25 21:10:18 +0700

    initial project

--- nodebeginer/next.config.js
+++ nodebeginer/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    env:{
+        HOST:'DB',
+        USER:'root',
+        PASSWORD:1234,
+        PORT_WEB:8000,
+    }
+}
 module.exports = nextConfig
